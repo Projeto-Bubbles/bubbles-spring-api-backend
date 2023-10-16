8c3fdf03fc63e0ec2f6a0035bea126255042d4be
PauloCezaretto <paulo.alvares@sptech.school>
2023-10-16 11:40:30 -0300

    [feature]: Including SpringBoot project

---
+++ src/main/java/bubbles/springapibackend/SpringApiBackendApplication.java
@@ -0,0 +1,13 @@
+package bubbles.springapibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApiBackendApplication.class, args);
+	}
+
+}
